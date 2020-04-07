64526c954cad528140e3f13eae523d4c9c9e319a
Lucas Amstalden <lucas-amstalden@hotmail.com>
2020-04-07 00:26:33 -0300

    subindo o projeto de crud MVC

---
+++ PrimeiroCrudMVC/UnitOfWork/UnitOfWorkApp.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Ajax.Utilities;
+using PrimeiroCrudMVC.Data;
+using PrimeiroCrudMVC.Models;
+using PrimeiroCrudMVC.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PrimeiroCrudMVC.UnitOfWork
+{
+    public class UnitOfWorkApp : DbContext
+    {
+        ContextApp context = new ContextApp();
+        Repository.Repository<Product> productRepository;
+
+        public Repository<Product> ProductRepository
+        {
+            get
+            {
+                if(productRepository == null)
+                {
+                    productRepository = new Repository<Product>(context);
+                }
+                return productRepository;
+            }
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+    }
+}
